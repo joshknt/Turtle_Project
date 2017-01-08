9eb23ed9179123b352d2421a065751ca0fd4db59
joshknt <joshknt@gmail.com>
2017-01-07 18:53:41 -0600

    Added serial output for testing
    Added support for serial output in record class and getter methods. Also
    main driver will print data as it recieves it to monitor.

--- src/turtleProject/Record.cpp
+++ src/turtleProject/Record.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include "Record.h"
 
 Record::Record()
@@ -21,3 +22,68 @@ Record::Record(int nID, int rn, int t, int h, int x, int y, int z)
 	yAcc = y;
 	zAcc = z;
 }
+
+int Record::getNestID()
+{ 
+  return nestID;  
+}
+
+
+unsigned long Record::getRecordNumber()
+{
+  return recordNumber;  
+}
+
+
+int Record::getTemp()
+{
+  return temp;
+}
+
+
+int Record::getHumidity()
+{
+  return humidity;
+}
+
+
+int Record::getXAcc()
+{
+  return xAcc;
+}
+
+
+int Record::getYAcc()
+{
+  return yAcc;
+}
+
+
+int Record::getZAcc()
+{
+  return zAcc;
+}
+
+
+void Record::printToSerial()
+{
+  Serial.print("NestID: "); 
+  Serial.println(this->nestID);
+  
+  Serial.print("Record #: "); 
+  Serial.println(this->recordNumber);
+  
+  Serial.print("Temp: "); 
+  Serial.println(this->temp);
+  
+  Serial.print("Humidity: "); 
+  Serial.println(this->humidity);
+  
+  Serial.print("X: ");
+  Serial.print(this->xAcc);
+  Serial.print(" Y: ");
+  Serial.print(this->yAcc);
+  Serial.print(" Z: ");
+  Serial.println(this->zAcc);
+}
+
