551bdfe26ebb6e7e41eb6f56d99217ad72682459
joshknt <joshknt@gmail.com>
2017-01-31 10:48:12 -0600

    Added offsets for accelerometer

--- TurtleMicrocontroller/turtleProject/Record.h
+++ TurtleMicrocontroller/turtleProject/Record.h
@@ -5,22 +5,21 @@
 class Record
 {
 	private:
-		int nestID;  //holds which nest it is in
-    int bottomTemp;
-    int middleTemp;
-    int topTemp;
+		uint8_t nestID;  //holds which nest it is in
+    uint16_t bottomTemp;
+    uint16_t middleTemp;
+    uint16_t topTemp;
 		int xAcc;
 		int yAcc;
 		int zAcc;
 
 	public:
 		Record();
-    Record(int nID);
-    Record(int nID, int bt, int mt, int tt, int x, int y, int z); //use this after DS18B20 is working
-    int getNestID();
-    int getBottomTemp() {return this->bottomTemp;}
-    int getMiddleTemp() {return this->middleTemp;}
-    int getTopTemp() {return this->topTemp;}
+    Record(uint8_t nID, uint16_t bt, uint16_t mt, uint16_t tt, int x, int y, int z); //use this after DS18B20 is working
+    uint8_t getNestID() {return this->nestID;}
+    uint16_t getBottomTemp() {return this->bottomTemp;}
+    uint16_t getMiddleTemp() {return this->middleTemp;}
+    uint16_t getTopTemp() {return this->topTemp;}
     int getXAcc() {return this->xAcc;}
     int getYAcc() {return this->yAcc;}
     int getZAcc() {return this->zAcc;}
