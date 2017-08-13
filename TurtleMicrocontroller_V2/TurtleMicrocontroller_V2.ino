dc1d47b686b585d13a45e3617a42598968640502
joshknt <joshknt@gmail.com>
2017-08-12 20:03:13 -0500

    Updated GNU GPL Licesnse

--- TurtleMicrocontroller_V2/TurtleMicrocontroller_V2.ino
+++ TurtleMicrocontroller_V2/TurtleMicrocontroller_V2.ino
@@ -27,7 +27,9 @@
  *    2017 Josh Kent and the MUTS team
  *    
  * License:
- *    This program is free software: you can redistribute it and/or modify
+ *    This file is part of TurtleMicrocontroller_V2.
+ *    
+ *    TurtleMicrocontroller_V2 is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
  *    the Free Software Foundation, either version 3 of the License, or
  *    (at your option) any later version.
